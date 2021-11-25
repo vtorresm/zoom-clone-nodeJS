aaf7abeed7dc2e8b0efeb669d05b750cf73bd37b
vtorresm <vatorresm88@gmail.com>
2021-11-25 18:00:53 -0500

    habilitar video

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const app = express();
 
 const server = require('http').Server(app);
 
-const { v4: uuidV4 } = require('uuid')
+const { v4: uuidV4 } = require('uuid');
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+  res.redirect(`/${uuidV4()}`);
+});
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+  res.render('room', { roomId: req.params.room });
+});
 
 server.listen(3030);
