2b9c630a37d13619f0ec44a831ccd044d21b95c4
vtorresm <vatorresm88@gmail.com>
2021-12-02 14:45:55 -0500

    Agregando nuevo usuario

--- public/script.js
+++ public/script.js
@@ -15,7 +15,15 @@ navigator.mediaDevices
     addVideoStream(myVideo, stream);
   });
 
-socket.emit('create or join');
+socket.emit('create or join', ROOM_ID);
+
+socket.on('user-connected', () => {
+  connecToNewUser();
+});
+
+const connecToNewUser = () => {
+  console.log('new user connected');
+}
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
