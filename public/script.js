c708a27c6c834fb7944b507e00e7ed68023416bc
vtorresm <vatorresm88@gmail.com>
2021-12-13 22:57:08 -0500

    Dandole funcionalidad a los botones

--- public/script.js
+++ public/script.js
@@ -31,6 +31,21 @@ navigator.mediaDevices
     socket.on('user-connected', (userId) => {
       connecToNewUser(userId, stream);
     });
+    // input value
+    let text = $('input');
+    // when press enter send message
+    $('html').keydown(function (e) {
+      if (e.which == 13 && text.val().length !== 0) {
+        socket.emit('message', text.val());
+        text.val('');
+      }
+    });
+    socket.on('createMessage', (message) => {
+      $('messages').append(
+        `<li class="message"><b>user</b><br/>${message}</li>`
+      );
+      scrollToBottom();
+    });
   });
 
 peer.on('open', (id) => {
@@ -51,4 +66,65 @@ const addVideoStream = (video, stream) => {
     video.play();
   });
   videoGrid.append(video);
+};
+
+const scrollToBottom = () => {
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop('scrollHeight'));
+};
+
+// Mute our video
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector('.main__mute_button').innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector('.main__mute_button').innerHTML = html;
+};
+
+const playStop = () => {
+  console.log('object');
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector('.main__video_button').innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector('.main__video_button').innerHTML = html;
 };
--- server.js
+++ server.js
@@ -27,7 +27,16 @@ io.on('connection', (socket) => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
   });
 });
 
-server.listen(3030);
+server.listen(process.env.PORT||3030);
